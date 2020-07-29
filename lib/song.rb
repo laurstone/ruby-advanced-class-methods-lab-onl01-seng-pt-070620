18a6f98fd83cd6c196071f8fadf4a65b44069f14
Lauren Stone <lstone321@gmail.com>
2020-07-29 04:41:22 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -41,7 +41,15 @@ class Song
    @@all.sort_by {|song| song.name}
   end
   
- 
+  def self.new_from_filename(filename)
+    rows = filename.split(" - ")
+    artist_name = rows[0]
+    name = rows[1].gsub(".mp3", "")
+    song = Song.new
+    song.name = name
+    song.artist_name = artist_name
+    song
+  end
  
  def self.create_from_filename(filename)
     rows = filename.split(" - ")
